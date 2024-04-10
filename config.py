0e083a5502ec16ba95750a0f099303cd886805f2
Ksjjsjdbh <166590002+Ksjjsjdbh@users.noreply.github.com>
2024-04-10 12:23:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-token = os.getenv('DISCORD_BOT_TOKEN')
-LogChannelID = 'LOG_CHANNEL_ID'
+token = os.getenv('MTIyMjk2NTI0NDIxMzU5NjI5MQ.GbXC94.T7wy4-nLV-4VTlVC0VT2GUh0j6xcQArRm93ftc')
+LogChannelID = '1223141773086429185'
 prefix = "!"
 status = 'Whitelist in action, only allowing trusted members.'
